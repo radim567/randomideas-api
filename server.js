a5168f8d38d7bfe9da0f44d994ea608f5342e5a6
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-06-30 15:20:03 +0200

    ideas input

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
 const app = express();
+
+const ideas = [
+  {
+    id: 1,
+    text: 'First Idea',
+    tag: 'technology',
+    username: 'John Doe',
+    date: '01-01-2020',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('Hello Express World!');
+});
+
+app.get('/api/ideas', (req, res) => {});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
